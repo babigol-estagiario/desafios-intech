5791aef0f5eff963e68a2b634eb9f50b9a4dbf80
babigol-estagiario <babigolestagiario@gmail.co>
2024-09-30 16:05:38 -0300

    Correção do Desafio

--- src/pages/desafio14/script.js
+++ src/pages/desafio14/script.js
@@ -1,14 +1,23 @@
-function isPangram(string){
-    const frase = prompt("Digite uma Frase")
-    letras = [
-      'a', 'b', 'c', 'd', 'e',
-      'f', 'g', 'h', 'i', 'j',
-      'k', 'l', 'm', 'n', 'o',
-      'p', 'q', 'r', 's', 't',
-      'u', 'v', 'w', 'x', 'y',
-      'z'
-    ];
-    const isPangram = letras.every(letra => frase.includes(letra));
-    console.log(isPangram ? "A Palavra é um Pangrama" : "A palavra não é um Pangrama");
+function isPangram(frase) {
+  const letras = [
+    'a', 'b', 'c', 'd', 'e',
+    'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y',
+    'z'
+  ];
+  
+  // Verifica se a frase contém todas as letras do alfabeto
+  const isPangram = letras.every(letra => frase.includes(letra));
+  
+  // Imprime e retorna o resultado
+  if (isPangram) {
+    console.log("A frase é um Pangrama");
+    return true;
+  } else {
+    console.log(`A frase não é um Pangrama (faltam letras): ${letras.filter(letra => !frase.includes(letra))}`);
+    return false;
   }
-  isPangram()
+}
+console.log(isPangram("The quicsmps over the lazy dog"));
